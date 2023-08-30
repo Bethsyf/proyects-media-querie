3738b656d58d382af19c192fa0d5cd4df53e186c
Bethsyf <bethsyfalcon@gmail.com>
2023-08-30 08:12:14 -0500

    Refactor: add image to box4

--- components/box4/Box4.tsx
+++ components/box4/Box4.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
+import Image from 'next/image';
 
 import s from './Box4.module.scss';
+import image from '../../public/image.png';
 
 const Box4 = (styles) => {
   return (
     <div className={`${styles} ${s.container}`}>
-      <b>
-        Caja4:{' '}
-        <i>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
-          dignissimos itaque tempore quisquam molestiae eius provident a minima
-          iste corporis est tempora commodi enim velit, sit unde harum facilis
-          magni!
-        </i>
-      </b>
+      <p>Caja4:</p> <Image src={image} alt="image" width={150} height={150} />
     </div>
   );
 };
